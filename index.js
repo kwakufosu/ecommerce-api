5a89c6c5a8d60b7bf03ca5efe402916d4f96945a
kwaku fosu <kwakuboachiefosu@gmail.com>
2021-12-11 17:44:36 +0000

    Added a user route and created some models

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const userRoute = require('./routes/user');
 
 dotenv.config();
 const app = express();
@@ -10,6 +11,9 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
+app.use(express.json());
+
+app.use('/api/users', userRoute);
 
 app.listen(process.env.PORT || 5000, () => {
   console.log('Server is running');
